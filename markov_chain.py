488c2842d86c590edc73035d24be41a7c2831e2d
Nicolás Martín Obesio <nicolas@MacBook-Air-de-Nicolas.local>
2017-03-16 20:41:02 -0300

    Generalization for n grams

--- markov_chain.py
+++ markov_chain.py
@@ -11,15 +11,19 @@ def strip_accents(s):
                   if unicodedata.category(c) != 'Mn')
 
 class SpecialChar:
-    pass
+    def __init__(self, name):
+        self.name = name
+    def __repr__(self):
+        return self.name
 
 
-open_token = SpecialChar()
-close_token = SpecialChar()
+open_token = SpecialChar("Open Token")
+close_token = SpecialChar("Close Token")
 
 class MarkovChain:
     def __init__(self):
         self.chain = {}
+        self.ngram = 1
 
     def train(self, corpora_address, author_id):
         import xml.etree.ElementTree
@@ -28,7 +32,7 @@ class MarkovChain:
             if author_elem.attrib['id'] == author_id:
                 author = author_elem
         self.name = author.attrib['name']
-
+        self.ngram = int(author.attrib['ngram'])
         # We tokenize the corpus.
         for text in author.findall('text'):
             text = text.text
@@ -39,34 +43,39 @@ class MarkovChain:
             # We remove tildes.
             text = strip_accents(text)
             # We remove all special characters.
-            text = re.sub(r"[^a-zA-Z0-9,.!?;:’'<>]", ' ', text)
+            text = re.sub(r"[^a-zA-Z0-9,.¡!¿?;:’'<>]", ' ', text)
             vectorized_text = [open_token] + [word.lower().strip() for word in text.split(' ') if word.strip() != ''] + [close_token]
-            for current_word, next_word in zip(vectorized_text, vectorized_text[1:]):
-                self.chain.setdefault(current_word, []).append(next_word)
+
+            for ngram in zip(*[vectorized_text[i:] for i in range(self.ngram+1)]):
+                self.chain.setdefault(ngram[:-1], []).append(ngram[-1])
 
 
-
-    def generate_quote(self, max_num_words = 25, ending_char = None):
+    def generate_quote(self, max_num_words = 50, ending_char = None):
         if not self.chain:
             raise MarkovChainError('The Markov Chain has not been trained.')
-        else:
-            current_word = random.choice(self.chain[open_token])
-            sentence = []
 
-            # We generate the text.
-            for _ in range(0, max_num_words):
-                if current_word is close_token:
-                    break
-                if current_word == ending_char:
-                    break
-                sentence.append(current_word)
 
-                if current_word in self.chain:
-                    current_word = random.choice(self.chain[current_word])
-                else:
-                    break
+        current_words = random.choice([ngram for ngram in self.chain.keys() if ngram[0] == open_token])
+        print(current_words)
+        sentence = [] + list(current_words[1:])
 
-            generated_text = ' '.join(sentence)
-            for char in ['.', ',', '!', '?']:
-                generated_text = generated_text.replace(' '+char, char)
-            return generated_text
+        # We generate the text.
+        for _ in range(0, max_num_words):
+            if current_words in self.chain:
+                next_word = random.choice(self.chain[current_words])
+                if next_word == close_token:
+                    break
+                sentence.append(next_word)
+                if next_word == ending_char:
+                    break
+                current_words =  tuple(sentence[-(self.ngram):])
+            else:
+                break
+
+        print(self.ngram)
+        generated_text = ' '.join(sentence)
+        for char in ['.', ',', '!', '?']:
+            generated_text = generated_text.replace(' '+char, char)
+        for char in ['¡', '¿']:
+            generated_text = generated_text.replace(char+' ', char)
+        return generated_text
